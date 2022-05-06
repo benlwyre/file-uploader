5843072ea8a92e29eba06cba23204f6c8b56a737
benlwyre <ben.l@sendwyre.com>
2022-05-06 15:05:17 -0700

    Upload to testwyre account endpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Component} from 'react';
+
+const auth = `Bearer ${process.env.REACT_APP_SECRET_KEY}`;
+const accountId = `${process.env.REACT_APP_ACCOUNT_ID}`;
+
+class App extends Component {
+
+	state = {
+
+	// Initially, no file is selected
+	selectedFile: null
+	};
+	
+	// On file select (from the pop up)
+	onFileChange = event => {
+	
+	// Update the state
+	this.setState({ selectedFile: event.target.files[0] });
+	
+	};
+	
+	// On file upload (click the upload button)
+	onFileUpload = async () => {
+	
+	// Details of the uploaded file
+	console.log(this.state.selectedFile);
+
+  const documentUrl = `https://api.testwyre.com/v3/accounts/${accountId}/individualGovernmentId?masqueradeAs=${accountId}&documentType=PASSPORT`;
+
+  console.log(documentUrl);
+	
+	const docUpload = await axios.post(documentUrl, 
+    this.state.selectedFile, 
+    {headers:{
+      'Content-Type': 'application/pdf',
+      'Authorization': auth}  
+    })
+  console.log(docUpload);
+	};
+	
+	// File content to be displayed after
+	// file upload is complete
+	fileData = () => {
+	
+	if (this.state.selectedFile) {
+		
+		return (
+		<div>
+			<h2>File Details:</h2>
+			
+<p>File Name: {this.state.selectedFile.name}</p>
+
+			
+<p>File Type: {this.state.selectedFile.type}</p>
+
+			
+<p>
+			Last Modified:{" "}
+			{this.state.selectedFile.lastModifiedDate.toDateString()}
+			</p>
+
+		</div>
+		);
+	} else {
+		return (
+		<div>
+			<br />
+			<h4>Choose before Pressing the Upload button</h4>
+		</div>
+		);
+	}
+	};
+	
+	render() {
+	
+	return (
+		<div>
+			<h1>
+			Wyre Document Upload Demo
+			</h1>
+			<h3>
+			File Upload using React
+			</h3>
+			<div>
+				<input type="file" onChange={this.onFileChange} />
+				<button onClick={this.onFileUpload}>
+				Upload!
+				</button>
+			</div>
+		{this.fileData()}
+		</div>
+	);
+	}
 }
 
 export default App;
